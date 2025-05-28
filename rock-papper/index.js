f7a5445c1272ae16c0f973b64aa9a035d4dfdcaa
prerak-26 <prerak2557@gmail.com>
2025-05-28 17:39:34 +1000

    css added

--- rock-papper/index.js
+++ rock-papper/index.js
@@ -1,6 +1,8 @@
 let button = document.querySelectorAll(".button");
+let scorePrint = document.querySelector(".score-print");
 let resultPrint = document.querySelector("#result");
 let resetScore = document.querySelector("#score");
+let resultShow = document.querySelector(".result-print");
 
 let score = JSON.parse(localStorage.getItem('score')) || {
     user: 0,
@@ -17,13 +19,13 @@ function selectComputerValue(randomValue) {
     let computerValue = '';
     switch (randomValue) {
         case 1:
-            computerValue = 'Rock';
+            computerValue = 'rock';
             break;
         case 2:
-            computerValue = 'Paper';
+            computerValue = 'paper';
             break;
         case 3:
-            computerValue = 'Scissors';
+            computerValue = 'scissors';
             break;
         default:
             break;
@@ -37,9 +39,9 @@ function resultGenerator(userValue, computerValue) {
         result = 'Draw';
         score.draw++;
     } else if (
-        (userValue === 'Rock' && computerValue === 'Scissors') ||
-        (userValue === 'Paper' && computerValue === 'Rock') ||
-        (userValue === 'Scissors' && computerValue === 'Paper')
+        (userValue === 'rock' && computerValue === 'scissors') ||
+        (userValue === 'paper' && computerValue === 'rock') ||
+        (userValue === 'scissors' && computerValue === 'paper')
     ) {
         result = 'You win';
         score.user++;
@@ -53,12 +55,14 @@ function resultGenerator(userValue, computerValue) {
 
 button.forEach(element => {
     element.onclick = function() {
-        const userValue = element.textContent;
+        const userValue = element.id;
+        console.log(userValue)
         const randomValue = valuGenerator();
         const computerValue = selectComputerValue(randomValue);
         let result = resultGenerator(userValue, computerValue);
-        resultPrint.innerText = `You: ${userValue}, computer: ${computerValue}. Result: ${result}
-        User: ${score.user}, Computer: ${score.computer}, Draw: ${score.draw}`;
+        resultPrint.innerText = result;
+        resultShow.innerHTML = `You <img src="assests/${userValue}-emoji.png" alt="" class="result-img"><img src="assests/${computerValue}-emoji.png" alt="" class="result-img"> Computer`
+        scorePrint.innerText = `You: ${score.user}, Computer: ${score.computer}, Draw: ${score.draw}`;
     }  
 });
 
@@ -67,6 +71,8 @@ resetScore.addEventListener("click", function(){
     score.computer = 0;
     score.draw = 0;
     localStorage.removeItem('score');
-    resultPrint.innerText = `User: ${score.user}, Computer: ${score.computer}, Draw: ${score.draw}`;
+    scorePrint.innerText = `You: ${score.user}, Computer: ${score.computer}, Draw: ${score.draw}`;
+    resultPrint.innerText = ``;
+    resultShow.innerHTML = ``
 });
 
