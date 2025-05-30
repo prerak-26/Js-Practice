c46a02dd8f1c96f881d8c7a681ee2ec128da3523
prerak-26 <prerak2557@gmail.com>
2025-05-30 17:20:58 +1000

    todo project

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -11,14 +11,14 @@ const toDo = JSON.parse(localStorage.getItem("item")) || [];
 
 renderToDo();
 
-function renderToDo(){
+function renderToDo() {
     let html = ``;
 
-   toDo.forEach((element, i) => {
-    const {name, date, time, done} = element;
-    const doneStyle = done ? `style="color:green"` : ``;
+    toDo.forEach((element, i) => {
+        const { name, date, time, done } = element;
+        const doneStyle = done ? `style="color:green"` : ``;
 
-    html += `<div class="todo-list" ${doneStyle}><p class="todo-list-name">${name}</p>
+        html += `<div class="todo-list" ${doneStyle}><p class="todo-list-name">${name}</p>
         <p class="todo-list-time">${date}</p>
         <p class="todo-list-time">${time}</p>
         <button class="btn-done" id="done${i}"><i class="ri-check-double-line"></i></button>
@@ -27,38 +27,40 @@ function renderToDo(){
     container.innerHTML = html;
 };
 
-function dateTimeFormate(dateTime){
+function dateTimeFormate(dateTime) {
     const [numberDate, time] = dateTime.split("T");
     const [year, month, day] = numberDate.split("-");
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     const date = `${day} ${months[parseInt(month) - 1]} ${year}`;
-    return [date,time];
+    return [date, time];
 };
 
 addToDo.addEventListener('click', () => {
     const name = todoValue.value;
     const dateTime = todoDateTime.value;
-    const [date,time] = dateTimeFormate(dateTime);
-    toDo.push({ name, date, time, done: false });
-    localStorage.setItem("item",JSON.stringify(toDo));
+    const [date, time] = dateTimeFormate(dateTime);
+    if (name !== `` && date && time) {
+        toDo.push({ name, date, time, done: false });
+        localStorage.setItem("item", JSON.stringify(toDo));
+    }
     todoValue.value = ``;
     todoDateTime.value = ``;
     renderToDo();
 });
 
 container.addEventListener('click', (e) => {
-    if (e.target.closest('.btn-delete')){
+    if (e.target.closest('.btn-delete')) {
         const deleteBtn = e.target.closest('.btn-delete');
-        const btnId = +deleteBtn.id.replace('delete',''); // + = parseInt(deleteBtn.id.replace('delete',''))
-        toDo.splice(btnId,1);
-        localStorage.setItem("item",JSON.stringify(toDo));
+        const btnId = +deleteBtn.id.replace('delete', ''); // + = parseInt(deleteBtn.id.replace('delete',''))
+        toDo.splice(btnId, 1);
+        localStorage.setItem("item", JSON.stringify(toDo));
     };
 
-    if(e.target.closest('.btn-done')){
+    if (e.target.closest('.btn-done')) {
         const doneBtn = e.target.closest('.btn-done');
-        const doneId = +doneBtn.id.replace('done','');
+        const doneId = +doneBtn.id.replace('done', '');
         toDo[doneId].done = true;
-        localStorage.setItem("item",JSON.stringify(toDo));
+        localStorage.setItem("item", JSON.stringify(toDo));
     };
 
     renderToDo();
