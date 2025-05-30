ba4211afa5f88b444884d8e28442b5bf6e01ee4f
prerak-26 <prerak2557@gmail.com>
2025-05-30 17:02:50 +1000

    todo project

--- rock-papper/index.js
+++ rock-papper/index.js
@@ -3,12 +3,53 @@ let scorePrint = document.querySelector(".score-print");
 let resultPrint = document.querySelector("#result");
 let resetScore = document.querySelector("#score");
 let resultShow = document.querySelector(".result-print");
+let autoPlay = document.querySelector(".auto-btn");
 
 let score = JSON.parse(localStorage.getItem('score')) || {
     user: 0,
     computer: 0,
     draw: 0
 }
+
+let intervalId;
+let isAutoPlyaing = false;
+
+autoPlay.addEventListener('click', function () {
+
+    if(autoPlay.innerText === `Auto Play`){
+        setTimeout(function(){
+            autoPlay.innerText = `Loading`;
+        },500);
+
+        setTimeout(function(){
+            autoPlay.innerText = `Stop Play`;
+        },4000);
+    } else{
+        setTimeout(function(){
+            autoPlay.innerText = `Auto Play`;
+        },500);
+    };
+
+
+    if (!isAutoPlyaing) {
+        intervalId = setInterval(function () {
+            const randomUserValue = valuGenerator();
+            const randomComputerValue = valuGenerator();
+            const autoUserMove = selectComputerValue(randomUserValue);
+            const autoComputerMove = selectComputerValue(randomComputerValue);
+            let autoResult = resultGenerator(autoUserMove, autoComputerMove);
+            resultPrint.innerText = autoResult;
+            resultShow.innerHTML = `You <img src="assests/${autoUserMove}-emoji.png" alt="" class="result-img"><img src="assests/${autoComputerMove}-emoji.png" alt="" class="result-img"> Computer`
+            scorePrint.innerText = `You: ${score.user}, Computer: ${score.computer}, Draw: ${score.draw}`;
+        }, 2000);
+        isAutoPlyaing = true;
+    } else {
+        clearInterval(intervalId);
+        isAutoPlyaing = false;
+        resultPrint.innerText = ``;
+        resultShow.innerHTML = ``
+    }
+});
 
 function valuGenerator() {
     const randomValue = Math.floor(Math.random() * 3) + 1;
@@ -54,7 +95,7 @@ function resultGenerator(userValue, computerValue) {
 }
 
 button.forEach(element => {
-    element.onclick = function() {
+    element.onclick = function () {
         const userValue = element.id;
         console.log(userValue)
         const randomValue = valuGenerator();
@@ -63,10 +104,10 @@ button.forEach(element => {
         resultPrint.innerText = result;
         resultShow.innerHTML = `You <img src="assests/${userValue}-emoji.png" alt="" class="result-img"><img src="assests/${computerValue}-emoji.png" alt="" class="result-img"> Computer`
         scorePrint.innerText = `You: ${score.user}, Computer: ${score.computer}, Draw: ${score.draw}`;
-    }  
+    }
 });
 
-resetScore.addEventListener("click", function(){
+resetScore.addEventListener("click", function () {
     score.user = 0;
     score.computer = 0;
     score.draw = 0;
